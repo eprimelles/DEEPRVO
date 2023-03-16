4a088445492db7b069020fd50ac74446882df250
ericPrimelles <eprimelles97@gmail.com>
2023-03-16 15:30:46 -0400

    Add state funcs

--- Env.py
+++ Env.py
@@ -5,7 +5,7 @@ from typing import Any
 class DeepNav():    
     def __init__(self, n_agents : int, scenario : int, discrete: bool = True, width : int = 255, height : int = 255, timestep : float = 0.25 , neighbor_dists : float = 1.0, 
                  time_horizont : float=10.0, time_horizont_obst : float = 20.0, radius : float=2.0, 
-                 max_speed : float=3.5) -> None:
+                 max_speed : float=3.5, opt : str = 'full') -> None:
         super().__init__()
         
         
@@ -48,20 +48,28 @@ class DeepNav():
 
             } 
    
-
+        self.opt = opt
     def act(self, actions):
         if self.discrete:
             return self.setDiscreteActions(actions)
         return self.setContActions(actions)
     
-    def getState(self, opt):
+    def getState(self):
+        
         return {
-            'full' : self.getMinimalState,
+            'full' : self.getFullState,
             'sensor' : self.getSensorialState,
             'min' : self.getMinimalState
-        }[opt]
+        }[self.opt]
 
-    
+    def reset(self):
+        self.success = False
+        
+        for i in range(self.n_agents):
+            self.sim.setAgentPosition(i, self.positions[i])
+        self.__episode_ended = False
+        self.__state = self.getState()()
+        return self.__state
 
     # Utility functions
     def normalize(self, x):
@@ -108,11 +116,80 @@ class DeepNav():
             self.sim.setAgentPrefVelocity(i, tuple(action))
 
     def getFullState(self):
-        pass
-
+        '''Return every agent position and pref vel'''
+        return [
+            self.getAgentFullState(i)
+            for i in range(self.n_agents)
+        ]
     def getSensorialState(self):
-        pass
+        return [
+            self.getAgentSensorialState(i)
+            for i in range(self.n_agents)
+        ]
     def getMinimalState(self):
-        pass 
+        return [
+            self.getAgentMinimalState(i)
+            for i in range(self.n_agents)
+        ]
+    def getAgentFullState(self, agent):
+        
+        state = []
+        state.append(self.sim.getAgentPosition(agent)[0])
+        state.append(self.sim.getAgentPosition(agent)[1])
+        state.append(self.sim.getAgentVelocity(agent)[0])
+        state.append(self.sim.getAgentVelocity(agent)[1])
 
-DeepNav(2, 0)
+        for  i in range(self.n_agents):
+            if i == agent:
+                continue
+
+            state.append(self.sim.getAgentPosition(i)[0])
+            state.append(self.sim.getAgentPosition(i)[1])
+            state.append(self.sim.getAgentVelocity(i)[0])
+            state.append(self.sim.getAgentVelocity(i)[1])
+        return state
+
+    def getAgentSensorialState(self, agent):
+        dirs = [0, 45, 90, 135, 180, 225, 270, 315]
+        a_pos = self.sim.getAgentPosition(agent)
+        
+        state = [None] * 10
+        state [0] = a_pos[0]
+        state [1] = a_pos[1]
+        positions = [self.sim.getAgentPosition(a) for a in range(self.n_agents) if a != agent]
+        for poss in positions:
+            x = a_pos[0] - poss[0]
+            y = a_pos[1] - poss[1]
+            ang = np.degrees(np.arctan(y/x))
+            
+            norm = np.linalg.norm((x, y))
+            ang_int = np.degrees(np.arcsin(self.radius/norm))
+            if np.isnan(ang_int):
+                continue
+            ang_inf = np.round((ang - ang_int) + 0.5)
+            ang_sup = np.round((ang + ang_int) - 0.5)
+            
+            ang_range = np.arange(ang_inf, ang_sup)
+            
+            # ang_range = list(range(ang_inf, ang_sup + 1))
+            for indx, a in enumerate(dirs):
+                if a in ang_range:
+                    state[indx + 2] = norm - self.radius
+        # Missing no collision measurements
+        for i in range(len(state)):
+            if state[i] is None:
+                x_dist = a_pos[0] - self.width
+                y_dist = a_pos[1] - self.height
+                state[i] = np.linalg.norm([x_dist, y_dist])
+        return state
+
+    def getAgentMinimalState(self, agent):
+        state = []
+        state.append(self.sim.getAgentPosition(agent)[0])
+        state.append(self.sim.getAgentPosition(agent)[1])
+        state.append(self.sim.getAgentVelocity(agent)[0])
+        state.append(self.sim.getAgentVelocity(agent)[1])
+        return state
+
+env = DeepNav(2, 0, opt='sensor')
+print(env.reset())
